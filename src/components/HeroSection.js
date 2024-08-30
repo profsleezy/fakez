1f25cc11578269028a2b44f4931b0afdb2bbe3f2
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-08-30 20:42:27 +0300

    herofinal

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,7 +11,7 @@ const HeroSection = () => {
       >
         Introducing Fakez
       </Chip>
-      <h1>FAKEZ.CC</h1>
+      <h1>Looking for a fansign that's almost<br /><span className="gradient-text"> too real?</span></h1>
       <p>Enhance your Discord experience with our innovative bot, Fakez, designed to create stunningly realistic fake fansigns. Leveraging cutting-edge AI technology, Fakez expertly crafts authentic-looking text and seamlessly integrates it onto high-quality images, elevating your digital interactions with a touch of creativity and flair. - ChatGPT</p>
       <Button variant="solid" color="primary" className="neon-button">
         Sign Up
