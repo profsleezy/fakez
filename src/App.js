4175c85f58d83e3fde893235195245403e117a1a
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-08-30 15:05:02 +0300

    reset

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-// src/App.js
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-      <Hero />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
