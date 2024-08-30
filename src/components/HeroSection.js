b896a3bbd53b4ef32c7d4f6f564781688f57dbda
SL55ZY <65545295+profsleezy@users.noreply.github.com>
2024-08-30 15:08:36 +0300

    hero

---
+++ src/components/HeroSection.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HeroSection.css';
+
+const HeroSection = () => {
+  return (
+    <header className="hero-section">
+      <h1>fakez.cc</h1>
+      <p>Welcome to fakez.cc, your go-to platform for all things fake but fun!</p>
+    </header>
+  );
+};
+
+export default HeroSection;
